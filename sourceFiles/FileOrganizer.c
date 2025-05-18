6ef51b5c5fe737fad24ca2057af040362997f491
Aadithya369 <aadithyaanandan123@gmail.com>
2025-05-18 10:50:38 +0530

    ask path from user

--- sourceFiles/FileOrganizer.c
+++ sourceFiles/FileOrganizer.c
@@ -2,16 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
-	char location[200];
-	fgets(location,sizeof(location),stdin);
-	char location2 [200];
-	snprintf(location2,"ls -1  %s",location);
+	char location[100];
+	char command[200];
 	char read_buffer[100];
 	char extension[20];
-	FILE *p1 = popen(location2,"r");	
+	printf("Enter the location:");
+	fgets(location,sizeof(location),stdin);
+	location[strcspn(location,"\n")] = '\0';
+	int flag;
+	char check_path[150];
+	snprintf(check_path,sizeof(check_path),"sh -c '[ -d \"%s\" ]'",location);
+	flag = system(check_path);
+	while(flag !=0){
+		printf("Please enter a valid path\n");
+		printf("Enter the location:");
+		fgets(location,sizeof(location),stdin);
+		location[strcspn(location,"\n")] = '\0';
+		snprintf(check_path,sizeof(check_path),"sh -c '[ -d \"%s\" ]'",location);
+		flag = system(check_path);
+	}
+	location[strcspn(location,"\n")] = '\0';
+	snprintf(command,sizeof(command),"ls -1  %s",location);
+	FILE *p1 = popen(command,"r");	
 	FILE *list = fopen("lists.txt","w+");
 	if(p1 == NULL){
-		perror("Error running the p1");
+		perror("Error running the process p1");
 		return 1;
 	}
 	while(fgets(read_buffer,sizeof(read_buffer),p1)){
@@ -20,6 +35,7 @@ int main(){
 	pclose(p1);
 	rewind(list);
 	while(fgets(read_buffer,sizeof(read_buffer),list)){
+		read_buffer[strcspn(read_buffer,"\n")] = '\0';
 		memset(extension,0,sizeof(extension));
 		read_buffer[strcspn(read_buffer,"\n")] = '\0';
 		int j = 0;
@@ -38,13 +54,13 @@ int main(){
 		}
 		extension[j] = '\0';
 		char command1[300];
-		snprintf(command1,sizeof(command1),"mkdir -p %s/%s",location2,extension);
+		snprintf(command1,sizeof(command1),"mkdir %s/%s",location,extension);
 		system(command1);
-		char command2[300];
-		snprintf(command2,sizeof(command2),"mv %s/*%s %s/%s",location2,location2,extension,extension);
+		char command2[3000];
+		snprintf(command2,sizeof(command2),"mv %s/%s %s/%s",location,read_buffer,location,extension);
 		system(command2);
 	}
-
+		printf("FILES ORGANIZED!!!");
 		fclose(list);
 		return 0;
 }
