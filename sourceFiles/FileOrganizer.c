9da073bfb449df1862f4eace2ca274e5cd7e03e9
Aadithya369 <aadithyaanandan123@gmail.com>
2025-05-21 08:33:04 +0530

    Add support to windows

--- sourceFiles/FileOrganizer.c
+++ sourceFiles/FileOrganizer.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#ifdef  _WIN32
+	#include <direct.h>
+#elif  __linux__
+	#include <dirent.h>
+	#include <sys/stat.h>
+	#include <unistd.h>
+	#include <sys/types.h>
+#endif
+
 int main(){
 	char location[100];
-	char command[200];
-	char read_buffer[100];
-	char extension[20];
-	printf("Enter the location:");
+	char file_name[200];
+	printf("PATH:");
 	fgets(location,sizeof(location),stdin);
 	location[strcspn(location,"\n")] = '\0';
-	int flag;
-	char check_path[150];
-	snprintf(check_path,sizeof(check_path),"sh -c '[ -d \"%s\" ]'",location);
-	flag = system(check_path);
-	while(flag !=0){
-		printf("Please enter a valid path\n");
-		printf("Enter the location:");
+	DIR *dir;
+	while((dir = opendir(location)) == NULL){
+		if(ENOENT == errno){
+			printf("Enter a valid path\n");
+		}
+		else{
+			printf("opendir() failed to open :(\n");
+		}
+		printf("PATH:");
 		fgets(location,sizeof(location),stdin);
 		location[strcspn(location,"\n")] = '\0';
-		snprintf(check_path,sizeof(check_path),"sh -c '[ -d \"%s\" ]'",location);
-		flag = system(check_path);
 	}
 	location[strcspn(location,"\n")] = '\0';
-	snprintf(command,sizeof(command),"ls -1  %s",location);
-	FILE *p1 = popen(command,"r");	
-	FILE *list = fopen("lists.txt","w+");
-	if(p1 == NULL){
-		perror("Error running the process p1");
+	struct dirent *files;
+	if(ENOENT == errno){
+		perror("Error opening dirent");	
 		return 1;
 	}
-	while(fgets(read_buffer,sizeof(read_buffer),p1)){
-		fputs(read_buffer,list);	
-	}
-	pclose(p1);
-	rewind(list);
-	while(fgets(read_buffer,sizeof(read_buffer),list)){
-		read_buffer[strcspn(read_buffer,"\n")] = '\0';
-		memset(extension,0,sizeof(extension));
-		read_buffer[strcspn(read_buffer,"\n")] = '\0';
+	while((files = readdir(dir)) != NULL){
+		char extension[20];
 		int j = 0;
 		int last_pos = -1;
-		for(int i = strlen(read_buffer) - 1;i >= 0;i--){
-			if(read_buffer[i] == '.'){
+		for(int i = strlen(files->d_name) - 1;i >= 0;i--){
+			if(files->d_name[i] == '.'){
 				last_pos = i;			
 				break;
 			}
@@ -49,18 +50,32 @@ int main(){
 		if (last_pos == -1){
 			continue;
 		}
-		for(int i = last_pos + 1;i < strlen(read_buffer);i++){
-			extension[j++] = read_buffer[i];
+		for(int i = last_pos + 1;i < strlen(files->d_name);i++){
+			extension[j++] = files->d_name[i];
 		}
 		extension[j] = '\0';
-		char command1[300];
-		snprintf(command1,sizeof(command1),"mkdir %s/%s 2>/dev/null",location,extension);
-		system(command1);
-		char command2[3000];
-		snprintf(command2,sizeof(command2),"mv %s/%s %s/%s 2>/dev/null",location,read_buffer,location,extension);
-		system(command2);
+
+		char new_path[300];
+		snprintf(new_path,sizeof(new_path),"%s/%s",location,extension);
+		#ifdef _WIN32
+			if(_mkdir(new_path) == -1){
+				perror("_mkdir() failed :(\n");
+				return 1;
+			}
+		#elif __linux__  
+			if(access(new_path,F_OK ) == -1){
+				if(mkdir(new_path,0755) == -1){
+					perror("mkdir failed :( ");
+					return 1;
+				}
+			}	
+		#endif
+		char command2[1024];
+		char old_path[1000];
+		snprintf(command2,sizeof(command2),"%s/%s/%s",location,extension,files->d_name);
+		snprintf(old_path,sizeof(old_path),"%s/%s",location,files->d_name);
+		rename(old_path,command2);
 	}
 		printf("FILES ORGANIZED!!!");
-		fclose(list);
 		return 0;
 }
