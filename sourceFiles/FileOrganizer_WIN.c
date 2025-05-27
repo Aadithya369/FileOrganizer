a0fc99d605f1fdd4703b19045560ca6aa7b302a0
Aadithya369 <aadithyaanandan123@gmail.com>
2025-05-27 15:36:49 +0530

    use stat for musl compatible

---
+++ sourceFiles/FileOrganizer_WIN.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+int main(){
+	char location[100];
+	printf("PATH:");
+	fgets(location,sizeof(location),stdin);
+	
+}
