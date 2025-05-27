f4dc2ca42f5b4d7fdeb0818cef6dffb68daa130b
Aadithya369 <aadithyaanandan123@gmail.com>
2025-05-27 07:46:14 +0530

    change buffer size

--- sourceFiles/FileOrganizer.c
+++ sourceFiles/FileOrganizer.c
@@ -18,7 +18,7 @@
 #endif
 
 int main(){
-	char location[50];
+	char location[500];
 	char file_name[30];
 	DIR *dir;
 	struct dirent *files;
@@ -38,10 +38,10 @@ int main(){
 	}
 	while((files = readdir(dir)) != NULL){
 		char *pos = strrchr(files->d_name,'.');
-		char dir_name[450];
-		char dir_path[550];
-		char old_path[450];
-		char new_path[850];
+		char dir_name[850];
+		char dir_path[1350];
+		char old_path[900];
+		char new_path[1700];
 		if(pos!=NULL){
 			if(files->d_name[0] == '.')
 				continue;
